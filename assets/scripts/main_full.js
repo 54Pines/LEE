2346c414753e6730d2a08d196821cd7ae8b58fc3
Aaron Barber <abarberaus@gmail.com>
2017-06-09 13:18:59 +0800

    form alert box copy fix

--- assets/scripts/main_full.js
+++ assets/scripts/main_full.js
@@ -442,7 +442,7 @@
   form.addEventListener('submit', function(event) { // listen for form submitting
           if (!event.target.checkValidity()) {
               event.preventDefault(); // dismiss the default functionality
-              alert('Please, fill the form'); // error message
+              alert('Please fill out the form.'); // error message
           }
       }, false);
 });
